2c4a2e77e454476b81a720301243a1c827a1bd6b
JhonEddi <31699155+JhonEddi@users.noreply.github.com>
2019-08-25 19:32:54 -0500

    Correciones de lectura

--- Figura.py
+++ Figura.py
@@ -1,3 +1,17 @@
 import areas
 print ("Calcular Áreas \n")
-print
+print ("1.Cuadrilatero \n2.Triangulo")
+
+x=int(input("Escoja Figura: "))
+
+if x==1:
+  L=int(input('Ingrese el lado: '))
+  Areas.area_Cuadrilatero(L)
+
+if x==2:
+  b=int(input('Ingrese la base: '))
+  h=int(input('Ingrese la altura: '))
+  Areas.area_triangulo(b,h)
+  
+if x>2:
+  return print ("No existe esta opción")
